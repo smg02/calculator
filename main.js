cf8220302851e853270cb08b729caa8bf8f5dfd7
Mohammed Ghouse <76863172+smg02@users.noreply.github.com>
2024-06-15 16:08:58 +0530

    done

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
-let firstOperand;
-let secondOperand;
-let operator;
+// Variables
+let operator = '';
+let currentNumber = '';
+let previousNumber = '';
 
-const add = (first, second) => first + second
-const subtract = (first, second) => first - second
-const multiply = (first, second) => first * second
-const divide = (first, second) => {
-    return (second == 0) ? "Divide By Zero Error" : first / second
-}
-
-const operate = (operator, firstOperand, secondOperand) => operator(firstOperand, secondOperand)
-
-const inputOne = document.querySelector(".one").value = "1";
-const inputTwo = document.querySelector(".two").textContent = "2";
-const inputThree = document.querySelector(".three").textContent = "3";
-const inputFour = document.querySelector(".four").textContent = "4";
-const inputFive = document.querySelector(".five").textContent = "5";
-const inputSix = document.querySelector(".six").textContent = "6";
-const inputSeven = document.querySelector(".seven").textContent = "7";
-const inputEight = document.querySelector(".eight").textContent = "8";
-const inputNine = document.querySelector(".nine").textContent = "9";
-const inputZero = document.querySelector(".zero").textContent = "0";
-const inputAdd = document.querySelector(".add").textContent = '+';
-const inputSubtract = document.querySelector(".subtract").textContent = "-";
-const inputMultiply = document.querySelector(".multiply").textContent = "x";
-const inputDivide = document.querySelector(".divide").textContent = "/";
-const inputEquals = document.querySelector(".equals").textContent = "=";
-const inputClear = document.querySelector(".clear").textContent = "Clear";
-
-let displayValue = document.querySelector(".display");
-
-let value = ""
-
-const getInput = (input) => {
-
-
-
-    if (input == inputAdd) {
-        if (!(firstOperand == null) || input == Number) input = secondOperand
-        firstOperand = value
-        value = ""
-        displayValue.textContent = ""
+const operate = (operator, previousNumber, currentNumber) => {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+    if (operator == '+') {
+        return (previousNumber += currentNumber);
+    } else if(operator == "-") {
+        return Math.round((previousNumber -= currentNumber));
+    } else if(operator == "x") {
+        return Math.ceil((previousNumber *= currentNumber));
     } else {
-        value = value + Number(input);
-        displayValue.textContent = value
+        return Math.round((previousNumber /= currentNumber) * 100000) / 100000;
+    }
+};
+
+// Populate Display
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const decimal = document.querySelector(".decimal");
+const equals = document.querySelector(".equals");
+
+const backspace = document.querySelector(".backspace");
+const display = document.querySelector(".display");
+const clear = document.querySelector(".clear");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        displayNumber(e.target.textContent);
+        display.textContent = currentNumber;
+    })
+})
+
+operators.forEach((op) => {
+    op.addEventListener("click", (e) => {
+        if (currentNumber != '' && previousNumber != '') {
+            display.textContent = operate(operator, previousNumber, currentNumber)
+        }
+        operator = e.target.textContent;
+        previousNumber = display.textContent;
+        currentNumber = '';
+        display.textContent = previousNumber;
+    })
+})
+
+equals.addEventListener("click", () => {
+    display.textContent = operate(operator, previousNumber, currentNumber)
     
-        console.log(value)
+})
+
+const displayNumber = (num) => {
+    if (currentNumber.length < 11) {
+        currentNumber += num;
     }
 }
 
+clear.addEventListener("click", () => {
+    display.textContent = '';
+    currentNumber = '';
+    previousNumber = '';
+    operator = '';
+})
+
+decimal.addEventListener("click", () => {
+    currentNumber += '.'
+    display.textContent = currentNumber
+})
+
+backspace.addEventListener("click", () => {
+    currentNumber = currentNumber.slice(0, -1);
+    display.textContent = currentNumber;
+})
+
+const dialog = document.querySelector("dialog");
+const modalClose = document.querySelector("#close");
+
+modalClose.addEventListener("click", () => {
+    dialog.remove()
+})
